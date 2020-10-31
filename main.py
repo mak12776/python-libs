8aa801f082262c6bf3c5075871aff3b0d9a6fbb9
Mohammad Amin Khakzadan <mak12776@gmail.com>
2020-10-31 22:35:04 +0330

    backup

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 import logging
 import sys
 
-import huffman
-
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(levelname)s: %(message)s')
 
-huffman.calculate_bytes('music_for_programming_49-julien_mier.mp3', 1)
+# huffman.calculate_bytes('music_for_programming_49-julien_mier.mp3', 2)
+file_bits = 695619664
+for bits_number in range(2, 20):
+    print(f'{file_bits:>10} / {bits_number:2} = {file_bits / bits_number:.2f}')
